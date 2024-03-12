825d9bdbcd818e70e541248e64b5de1a0bc6e1a6
whataten <tiktaalik135462@gmail.com>
2024-03-12 12:51:42 +0000

    change project type from gradle to spring

---
+++ src/main/java/ten/exam_mate/ExamMateApplication.java
@@ -0,0 +1,13 @@
+package ten.exam_mate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamMateApplication.class, args);
+	}
+
+}
