06acbdd3d3ae59ffbb9e062ab356b467975a19d3
whataten <tiktaalik135462@gmail.com>
2024-09-06 11:03:19 +0900

    add statement to response object for ignoring new claims

--- src/main/java/ten/exam_mate/APIResponse/MessageDTO.java
+++ src/main/java/ten/exam_mate/APIResponse/MessageDTO.java
@@ -1,7 +1,9 @@
 package ten.exam_mate.APIResponse;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class MessageDTO {
     @JsonProperty("role")
     private String role;
